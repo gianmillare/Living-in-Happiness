ce32a1417b8c26b06c242d4578c4c2b7d44acd8b
gianmillare <gianmillaregsm@gmail.com>
2021-06-20 18:25:59 -0700

    created streamlit app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("World Happiness Analysis")
+st.markdown("### An analysis on the 2021 World Happiness Report. In this app, I analyze the commonalities of the top ten" 
+            " happiest countries, and provide statistics on constituents of a country's happiness score.")
